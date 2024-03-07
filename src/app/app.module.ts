1d7708fbb72105f021e2d25de4345751569513c4
spaniardmaximus <israel.conesa@paxton-access.co.uk>
2024-03-07 11:24:10 +0000

    first blood

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { IsraelComponent } from './israel/israel.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
     ShoppingListEditComponent,
     RecipeDetailComponent,
     WelcomeComponent,
+    IsraelComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/israel/israel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-israel',
+  templateUrl: './israel.component.html',
+  styleUrl: './israel.component.css'
+})
+export class IsraelComponent {
+
+}
