4c12f1cf095b9d82fa65cde495672dc4301fb7fe
carl-meyer-paxton <carl.meyer@paxton.co.uk>
2024-03-07 11:19:41 +0000

    commit only the component.ts file

---
+++ src/app/carl/carl.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carl',
+  templateUrl: './carl.component.html',
+  styleUrl: './carl.component.css'
+})
+export class CarlComponent {
+
+}
